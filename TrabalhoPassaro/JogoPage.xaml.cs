1917e54d3299224c56c545ea3b07385475a3c729
julia <jsantana23julia@gmail.com>
2024-10-11 10:56:20 -0300

    juju

--- TrabalhoPassaro/JogoPage.xaml.cs
+++ TrabalhoPassaro/JogoPage.xaml.cs
@@ -2,11 +2,34 @@ namespace TrabalhoPassaro;
 
 public partial class JogoPage : ContentPage
 {
-	
+	const int gravidade = 30;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = false;
+    private object fotopassaro;
 
-	public JogoPage()
+    public JogoPage()
 	{
 		InitializeComponent();
 	}
+	void AplicaGravidade()
+	{
+		imgpassaro.TranslationY+=gravidade;
+	}
+
+	async Task Desenhar()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenhar();
+	}
+
+
 
 }
--- TrabalhoPassaro/MainPage.xaml.cs
+++ TrabalhoPassaro/MainPage.xaml.cs
@@ -2,16 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	
+
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private async void iniciarclicado(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new JogoPage());
-        }
+	private async void BotaoIniciarFoiClicado(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new JogoPage());
+	}
 }
 
