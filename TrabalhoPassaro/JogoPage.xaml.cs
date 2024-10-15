5c29266d5241a3ca6b312d315b857cd72b584f50
jsantana23julia@gmail.com <jsantana23julia@gmail.com>
2024-10-15 10:28:21 -0300

    julia 15/10

--- TrabalhoPassaro/JogoPage.xaml.cs
+++ TrabalhoPassaro/JogoPage.xaml.cs
@@ -2,12 +2,15 @@ namespace TrabalhoPassaro;
 
 public partial class JogoPage : ContentPage
 {
-	const int gravidade = 30;
-	const int tempoEntreFrames = 25;
-	bool estaMorto = false;
-    private object fotopassaro;
+   const int gravidade=30;
+   const int tempoEntreFrames=25;
+   bool estaMorto=false;
+   double larguraJanela=0;
+   double alturaJanela=0;
+   int velocidade=20;
 
-    public JogoPage()
+
+		public JogoPage()
 	{
 		InitializeComponent();
 	}
@@ -16,20 +19,46 @@ public partial class JogoPage : ContentPage
 		imgpassaro.TranslationY+=gravidade;
 	}
 
-	async Task Desenhar()
-	{
+     async Task Desenhar()
+	 {
 		while (!estaMorto)
 		{
 			AplicaGravidade();
 			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
 		}
-	}
-	protected override void OnAppearing()
+	 }
+   
+
+	protected override void OnSizeAllocated(double w, double h)
 	{
-		base.OnAppearing();
+		base.OnSizeAllocated(w, h);
+		larguraJanela=w;
+		alturaJanela=h;
+	}
+
+      void GerenciaCanos()
+	{
+		CanoDeCima.TranslationX-=velocidade;
+		CanoDeBaixo.TranslationX-=velocidade;
+		if(CanoDeBaixo.TranslationX<=-larguraJanela)
+	 {
+		CanoDeBaixo.TranslationX=0;
+		CanoDeCima.TranslationX=0;
+
+     }
+  
+	}
+	void OnGameOverClicked(object s, TappedEventArgs a)
+    {
+		frameGameOver.IsVisible=false;
+		Inicializar();
 		Desenhar();
 	}
-
-
+	void Inicializar()
+	{
+		estaMorto=false;
+	    imgpassaro.TranslationY=0;
+	}
 
 }
